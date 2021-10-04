755ec7aa1158f2ffabcd5dd0db077bc1ccc49006
sujaysingh1001 <84987073+sujaysingh1001@users.noreply.github.com>
2021-10-04 14:20:49 +0530

    Update and rename .py to calculator.py

--- calculator.py
+++ calculator.py
@@ -24,7 +24,7 @@ print("Please select operation -\n" \
   
   
 # Take input from the user 
-select = int(input("Select operations form 1, 2, 3, 4 :"))
+select = int(input("Select operations form 1, 2, 3, 4 :"))))
   
 number_1 = int(input("Enter first number: "))
 number_2 = int(input("Enter second number: "))
